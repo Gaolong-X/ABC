f7321ec573d66572a902cbb319c7626fe7e90e53
Gaoloog <1025264711@qq.com>
2017-11-17 15:56:28 +0800

    multiple log file compression

--- nginx_log_backup.sh
+++ nginx_log_backup.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-NGINX_PID="/var/run/nginx.pid"
-
+NGX_PID="/var/run/nginx.pid"
+NGX_LOG_DIR="/usr/local/nginx/logs"
+TARGET_DIR="/usr/local/nginx/logs"
+DATE=$(date +%Y_%m_%d)
+TARGET_FILE_EXT="${DATE}.log"
+ARCHIVE_FILE_EXT=".log.tar.gz"
 #access log
-ACCESS_LOG="/data/logs/nginx/access.log"
+LOGS=(
+	"access.log"
+	"error.log"
+	"web.server.org.access.log"
+	"web.server.org.error.log"
+)
 
-#archive target dir
-TARGET_DIR="/data/logs/nginx"
+target_log_arr=()
+for log in ${LOGS[*]}
+do	
+	target_log_name=${log%.log*}_${TARGET_FILE_EXT}
+	target_log_arr+=(${target_log_name})
+	mv ${NGX_LOG_DIR}/${log} ${TARGET_DIR}/${target_log_name}
+done
 
-#archive name
-TARGET_FILE_NAME="${TARGET_DIR}/access_$(date -d "yesterday" +%Y_%m_%d).log"
+kill -USR1 $(cat ${NGX_PID})
 
-ARCHIVE_FILE_NAME="${TARGET_DIR}/access_$(date -d "yesterday" +%Y_%m_%d).log.tar.gz"
+cd ${TARGET_DIR}
 
-mv ${ACCESS_LOG} ${TARGET_FILE_NAME}
+for log in ${target_log_arr[*]}
+do
+	target_log_name=${log%.log*}${ARCHIVE_FILE_EXT}
+	tar -zcf ${target_log_name} ${log} --remove-files		
+done
 
-kill -USR1 $(cat ${NGINX_PID})
-
-tar -zcf ${TARGET_FILE_NAME} ${ARCHIVE_FILE_NAME} --remove-files
