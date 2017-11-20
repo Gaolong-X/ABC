094712d60bb036c1386c470b251157e9af5cb9d9
Gaoloog <1025264711@qq.com>
2017-11-20 11:33:01 +0800

    update

--- install_php.sh
+++ install_php.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-PHP_VERSION="5.6.28"
+PHP_VERSION="5.6.32"
 PHP_DOWNLOAD_DIR="/tmp/php"
 PHP_INSTALL_DIR="/usr/local/php-${PHP_VERSION}"
+PHP_DOWNLOAD_URL="http://cn2.php.net/distributions/php-${PHP_VERSION}.tar.gz"
 
 #install php lib
-rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm && \
+#rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm && \
     yum install -y gcc-c++ \
     zlib \
     zlib-devel \
@@ -25,12 +26,21 @@ rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 #init some dir
 groupadd www && useradd -g www www
 #create tmp dir
-mkdir ${PHP_DOWNLOAD_DIR}
-#Make install php
-cd ${PHP_DOWNLOAD_DIR} && \
-    wget http://cn2.php.net/distributions/php-${PHP_VERSION}.tar.gz && \
-    tar -zxvf php-${PHP_VERSION}.tar.gz && \
-    cd php-${PHP_VERSION} && \
+if [ ! -d ${PHP_DOWNLOAD_DIR} ]
+then
+    mkdir ${PHP_DOWNLOAD_DIR}
+fi
+
+cd ${PHP_DOWNLOAD_DIR}
+
+if [ ! -f php-${PHP_VERSION}.tar.gz ]
+then
+     wget ${PHP_DOWNLOAD_URL}
+fi
+
+ tar -zxvf php-${PHP_VERSION}.tar.gz
+
+cd php-${PHP_VERSION} && \
     ./configure --prefix=${PHP_INSTALL_DIR} \
     --with-config-file-path=${PHP_INSTALL_DIR}/etc \
     --with-config-file-scan-dir=${PHP_INSTALL_DIR}/etc/php.d \
@@ -73,7 +83,7 @@ cd ${PHP_DOWNLOAD_DIR} && \
     --enable-ipv6 \
     --disable-debug \
     --without-pear && \
-    make && make install
+    make -j && make install
 
 #set php-fpm
 cp sapi/fpm/init.d.php-fpm.in /etc/init.d/php-fpm
