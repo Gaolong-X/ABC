ae285efc28cedc0ac60dbc0bf4724953e9c1e7d1
Gaoloog <1025264711@qq.com>
2017-12-11 15:43:41 +0800

    nat tpl

--- iptables.sh
+++ iptables.sh
@@ -17,4 +17,7 @@ iptables -A INPUT -p icmp -j ACCEPT
 
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -m state --state NEW -m tcp -p tcp -m multiport --dports 80,443 -j ACCEPT
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 3306 -j ACCEPT
+
+#NAT转发  
+#iptables -t nat -A PREROUTING -p tcp --dport 33666 -j DNAT --to-destination 192.168.32.101:3306
+#iptables -t nat -A POSTROUTING -d 192.168.32.101 -p tcp --dport 3306 -j SNAT --to-source 192.168.32.100
